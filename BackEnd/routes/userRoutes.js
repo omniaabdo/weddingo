6e67636524ede4991d351a95498a2c071db2a788
omniaabdo <33305849+omniaabdo@users.noreply.github.com>
2024-10-04 20:36:41 +0300

    Delete BackEnd directory dupplicated

--- BackEnd/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { register, login } = require('../controllers/userController');
-const { validateUser, validate } = require('../middleware/validator');
-const router = express.Router();
-
-router.post('/register', validateUser, validate, register);
-router.post('/login', login);
-
-module.exports = router;
